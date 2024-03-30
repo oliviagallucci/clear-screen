503d242ff4eb9bd00b8f828035c7580f58a3bf5b
Erez <22354670+ErezBinyamin@users.noreply.github.com>
2024-03-30 18:46:30 -0400

    Update clear-screen.py
    Added verbose docstrings

--- clear-screen.py
+++ clear-screen.py
@@ -1,15 +1,39 @@
+"""
+Help on clear-screen.py:
+
+NAME
+    clear-screen.py
+
+DESCRIPTION
+    clear-screen is a Python module that detects the host operating system
+    and will clear a terminals text history when the clear_screen() function
+    is called.
+
+    See https://github.com/oliviagallucci/clear-screen for complete documentation
+
+PACKAGE CONTENTS
+    clear_screen()
+"""
 import os
 
- def clear_screen():
-        # https://github.com/python/cpython/blob/main/Lib/os.py#L6
-        if os.name == 'nt':
-            os.system('cls')
+def clear_screen():
+   """
+   clear_screen uses os.name to detect the operating system then
+   uses os.system to execute the appropriate terminal command for clearing the screen
 
-        elif os.name == 'posix':
-            os.system('printf "\033c"') # https://stackoverflow.com/questions/24754406/how-can-you-clear-reset-the-screen-in-unix-posix-not-curses-newlines
-        else:
-            # unsupported OS
-            print("Unsupported OS - feel free to add support for this OS at ")
-            print("https://github.com/oliviagallucci/clear-screen/")
-            return
+   Args:
+       None
+   Returns:
+       Success == True
+   """
+   # https://github.com/python/cpython/blob/main/Lib/os.py#L6
+   if os.name == 'nt':
+      os.system('cls')
+   elif os.name == 'posix':
+      os.system('printf "\033c"') # https://stackoverflow.com/questions/24754406/how-can-you-clear-reset-the-screen-in-unix-posix-not-curses-newlines
+   else:
+      print("Unsupported OS - feel free to add support for this OS at ")
+      print("https://github.com/oliviagallucci/clear-screen/")
+      return False
+   return True
           
