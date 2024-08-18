a80dacd053364743968170fc8c46f71218c772da
Erez Binyamin <ezbin@pm.me>
2024-08-18 14:43:19 -0400

    clear_screen.py: Prints error message to stderr instead of printing to stdout

--- clear_screen.py
+++ clear_screen.py
@@ -32,8 +32,8 @@ def clear_screen():
    elif os.name == 'posix':
       os.system('printf "\033c"') # https://stackoverflow.com/questions/24754406/how-can-you-clear-reset-the-screen-in-unix-posix-not-curses-newlines
    else:
-      print("Unsupported OS - feel free to add support for this OS at ")
-      print("https://github.com/oliviagallucci/clear-screen/")
+      print("Unsupported OS - feel free to add support for this OS at ", file=sys.stderr)
+      print("https://github.com/oliviagallucci/clear-screen/", file=sys.stderr)
       return False
    return True
 
